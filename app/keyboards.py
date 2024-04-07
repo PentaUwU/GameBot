9317dc24b8281a521b31f940319b681af090173e
IvanCringe <growstreet31@gmail.com>
2024-04-07 18:04:33 +0300

    keyboard_test

--- app/keyboards.py
+++ app/keyboards.py
@@ -3,7 +3,17 @@ from aiogram.types import (InlineKeyboardMarkup, InlineKeyboardButton)
 
 
 #-----------------------------------КНОПКИ-----------------------------
-btn_ =  InlineKeyboardButton(text='')
+#Кнопка назад
+btn_back = InlineKeyboardButton(text='Вернуться назад', callback_data='btn_back')
+
+#Кнопки главного меню
+btn_swag =  InlineKeyboardButton(text='Хуй', callback_data='btn_swag')
+btn_swag2 =  InlineKeyboardButton(text='Хуй', callback_data='btn_swag2')
+btn_swag3 =  InlineKeyboardButton(text='Хуй', callback_data='btn_swag3')
+btn_swag4 =  InlineKeyboardButton(text='Хуй', callback_data='btn_swag4')
+btn_swag5 =  InlineKeyboardButton(text='Хуй', callback_data='btn_swag5')
 
 
 #-----------------------------------КЛАВИАТУРЫ-----------------------------------
+#Клавиатура главного меню
+kb_main = InlineKeyboardMarkup([[btn_swag,btn_swag2], [btn_swag3, btn_swag4][btn_swag5]])
