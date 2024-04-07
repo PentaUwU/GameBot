fdfc99cf0100422118d0c26e1f33ed21b043d43d
PentaUwU <posohov4@gmail.com>
2024-04-07 18:08:00 +0300

    Database

---
+++ app/database/models.py
@@ -0,0 +1,15 @@
+from peewee import *
+
+
+db = SqliteDatabase('./app/database/data.db')
+
+class BaseModel(Model):
+    id = AutoField()
+    class Meta:
+        database = db
+
+class User(BaseModel):
+    user_id = BigIntegerField(primary_key=True)
+    username = CharField(null = False)
+    class Meta:
+        db_table = 'Users'
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from app.handlers import router
+from app.database.models import db, User
 from config import TOKEN
 
 
@@ -14,6 +15,10 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
+    db.connect()
+    db.create_tables([User])
+    db.close()
+
     logging.basicConfig(level = logging.INFO)
     try:
         asyncio.run(main())
