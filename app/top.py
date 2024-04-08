f3d8e1fde06f5544eef6ca7dee618fe14c63af0d
PentaUwU <posohov4@gmail.com>
2024-04-08 14:12:48 +0300

    top update

--- app/top.py
+++ app/top.py
@@ -3,12 +3,23 @@ from app import router
 from aiogram.types import CallbackQuery, Message
 from app.database.models import User
 import app.keyboards as kb
+from app.level import ff, lvl_plus
 
 
 
 @router.callback_query(F.data == 'btn_top')
 async def top_play(callback: CallbackQuery):
+    user = User.select().where(User.user_id == callback.from_user.id).first()
+    #Определяет new_level чтобы в переменной был просто нови уровень типочка
+    new_level = lvl_plus(user.user_xp, user.user_lvl)
+    while user.user_xp >= ff(new_level):
+        new_level += 1
+    #Если новый уровень из цикла блять больше пользовательского просто заменяет его в бд
+    if new_level > user.user_lvl:
+        user.user_lvl = new_level
+        user.save()
     users = User.select().order_by(User.user_lvl.desc).limit(2)
+    
     
     # user_list = [users]
 
