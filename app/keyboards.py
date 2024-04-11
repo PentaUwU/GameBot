273d97bbdd2a05ed156aa2a9e8474aaca2353542
PentaUwU <posohov4@gmail.com>
2024-04-11 14:26:13 +0300

    group button

--- app/keyboards.py
+++ app/keyboards.py
@@ -14,6 +14,7 @@ btn_daily_bonus =  InlineKeyboardButton(text='Ежедневный бонус', 
 btn_top_lvl =  InlineKeyboardButton(text='➡️', callback_data='btn_top_lvl')
 btn_top_balance =  InlineKeyboardButton(text='⬅️', callback_data='btn_top_balance')
 btn_transfer = InlineKeyboardButton(text = "Перевод денег", callback_data="btn_transfer")
+btn_group = InlineKeyboardButton(text = "Меню групп", callback_data='btn_group')
 btn_back_transfer = InlineKeyboardButton(text="Вернуться назад", callback_data= "btn_transfer")
 
 #Кнопка накрутки уровня для проверки
@@ -41,7 +42,7 @@ kb_back = InlineKeyboardMarkup(inline_keyboard=[[btn_back]])
 kb_main = InlineKeyboardMarkup(inline_keyboard=[[btn_profile, btn_daily_bonus],[btn_top, btn_work],[btn_cheat]])
 kb_top_balance = InlineKeyboardMarkup(inline_keyboard=[[btn_back, btn_top_lvl]])
 kb_top_lvl = InlineKeyboardMarkup(inline_keyboard=[[btn_top_balance, btn_back]])
-kb_profile = InlineKeyboardMarkup(inline_keyboard=[[btn_transfer, btn_back]])
+kb_profile = InlineKeyboardMarkup(inline_keyboard=[[btn_transfer, btn_back, btn_group]])
 kb_transfer =InlineKeyboardMarkup(inline_keyboard=[[btn_back_profile]])
 kb_back_transfer = InlineKeyboardMarkup(inline_keyboard=[[btn_back_transfer]])
 kb_confirm = InlineKeyboardMarkup(inline_keyboard=[[btn_confirm, btn_back_transfer]])
